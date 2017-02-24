7cc08c7c93430e025a43ccc9be936e61a716f135
joeseiden <joe.seiden214@gmail.com>
2017-02-23 16:25:04 -0800

    Finish absurdBubbleSort

--- callback_exercises.js
+++ callback_exercises.js
@@ -57,4 +57,56 @@ function addNumbers(sum, numsLeft, completionCallback){
   }
 }
 
-addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
+// addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
+
+//absurdBubbleSort
+
+function askIfGreaterThan(el1, el2, callback) {
+  reader.question(`Is ${el1} greater than ${el2}?\n`, function(answer){
+    const ans = answer.toLowerCase();
+    if (ans === "yes") {
+      callback(true);
+      // reader.close();
+    } else if (ans === "no") {
+      callback(false);
+      // reader.close();
+    } else {
+      console.log("UserError: ID10T (see documentation)");
+      reader.close();
+    }
+  });
+}
+
+// askIfGreaterThan(2, 5, ans => console.log(`${ans}`));
+
+function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
+  if (i < (arr.length - 1)){
+    askIfGreaterThan(arr[i], arr[i + 1], isGreaterThan => {
+      if(isGreaterThan){
+        const oldPos = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = oldPos;
+        madeAnySwaps = true;
+      }
+      innerBubbleSortLoop(arr, i + 1, madeAnySwaps, outerBubbleSortLoop);
+    });
+  } else if (i == (arr.length - 1)) {
+    outerBubbleSortLoop(madeAnySwaps);
+  }
+}
+
+function absurdBubbleSort(arr, sortCompletionCallback) {
+  function outerBubbleSortLoop(madeAnySwaps) {
+    if(madeAnySwaps){
+      innerBubbleSortLoop(arr, 0, false, outerBubbleSortLoop);
+    } else {
+      sortCompletionCallback(arr);
+    }
+  }
+  outerBubbleSortLoop(true);
+}
+
+absurdBubbleSort([3, 4, 2, 1], function(arr){
+  console.log("Sorted array: " + JSON.stringify(arr));
+  reader.close();
+});
